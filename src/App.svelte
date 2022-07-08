f0917e95ad7804aa4084169b16e74759b0327b6d
LaureanoVera <laureanoigvera@gmail.com>
2022-07-08 09:21:58 -0300

    endpoints and query parameters

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  // import { getData } from "./components/GetAPI.svelte";
+
+  const URL = "https://api.thecatapi.com/v1/images/search?limit=10";
+
+  let info = [];
+
+  const getData = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    info = [...data];
+  };
+
+  getData();
+</script>
+
+<main>
+  <section>
+    {#each info as item}
+      <article>
+        <img src={item.url} alt={item.id} />
+      </article>
+    {/each}
+  </section>
+  <button on:click={getData}>Refresh</button>
+</main>
+
+<style>
+  img {
+    height: 250px;
+    width: 250px;
+    object-fit: cover;
+  }
+</style>
---
+++ src/components/GetAPI.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  export async function getData(urlApi) {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+
+    return [...data];
+  }
+</script>
